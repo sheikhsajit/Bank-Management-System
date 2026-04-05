afd398bdb271eb8609759c2b7146010b0597b9b1
Sheikh Sajit <114417876+sheikhsajit@users.noreply.github.com>
2026-04-06 00:21:39 +0600

    DbContext

---
+++ BMS/Data/ApplicationDbContext.cs
@@ -0,0 +1,28 @@
+using BMS.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BMS.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<User> Users => Set<User>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>(entity =>
+            {
+                entity.HasKey(u => u.Id);
+                entity.HasIndex(u => u.Username).IsUnique();
+                entity.HasIndex(u => u.Email).IsUnique();
+                entity.Property(u => u.CreatedAt).HasDefaultValueSql("GETUTCDATE()");
+            });
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
